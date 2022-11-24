23fa3c0fd3cc2535317b934a7317c04bd38f1c0a
Aaron <22418751+ajvsol@users.noreply.github.com>
2022-11-24 16:29:11 +0000

    retro time tests done

--- my-app/src/components/SubmitButton/SubmitButton.test.js
+++ my-app/src/components/SubmitButton/SubmitButton.test.js
@@ -1,19 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { test, expect, describe } from "@jest/globals";
 import Component from "./index";
 import { pageTypes } from "../../data/pageTypes";
 
 describe("submit button renders on all pages", () => {
+  const props = {
+    onClick: jest.fn()
+  };
+
   let currentPage = "";
+
   beforeEach(() => {
     for (let page of pageTypes) {
       currentPage = page;
       //console.log(currentPage);
     }
   });
+
   test(`Submit button renders on the ${currentPage}`, () => {
     render(<Component pageType={currentPage} />);
     const actual = screen.getByRole("button");
     expect(actual).toBeInTheDocument();
   });
+
 });
