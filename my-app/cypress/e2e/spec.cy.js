824bf2b06739f8a6150fd900046e5bf5204f80d1
Gabriel Brocklesby <gabrielbrocklesby@Gabriels-MacBook-Pro.local>
2022-11-29 16:57:53 +0000

    further seperation of cypress tests from temp list and adding description

--- my-app/cypress/e2e/spec.cy.js
+++ my-app/cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 describe('visits the page', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000') 
+    cy.viewport(5120, 2880);
     /* ==== Generated with Cypress Studio ==== */
   })
 })
@@ -38,34 +39,52 @@ describe('takes the input number', () => {
     })
 
 
-
-
-
-    describe('temp', () => {
-      it('temp', () => {
-      
+    describe('input options', () =>{
+      it('user can input text into input field', () =>{
         cy.get('[name="input-1"]').clear('M');
         cy.get('[name="input-1"]').type('Mushrooms ');
         cy.get('[name="input-2"]').clear('p');
         cy.get('[name="input-2"]').type('pepperoni');
         cy.get('[name="input-3"]').clear('c');
         cy.get('[name="input-3"]').type('cheese');
+      })
+      it('when button clicked', () =>{
         cy.get('[data-testid="options-input"]').click();
-        cy.get(':nth-child(1) > :nth-child(2) > input').click({force: true});
-        cy.get(':nth-child(1) > :nth-child(2) > input').click({force: true});
-        cy.get(':nth-child(2) > :nth-child(2) > input').clear('h');
-        cy.get(':nth-child(2) > :nth-child(2) > input').type('hello');
-        cy.get(':nth-child(2) > :nth-child(2) > .custom-select').select('Pro');
-        cy.get(':nth-child(2) > :nth-child(3) > .custom-select').select('Con');
-        cy.get(':nth-child(2) > :nth-child(3) > input').clear('n');
-        cy.get(':nth-child(2) > :nth-child(3) > input').type('nope');
-        cy.get(':nth-child(3) > :nth-child(2) > .custom-select').select('Pro');
-        cy.get(':nth-child(3) > :nth-child(2) > input').clear('fd');
-        cy.get(':nth-child(3) > :nth-child(2) > input').type('fdgrefdg');
-        cy.get(':nth-child(3) > :nth-child(3) > .custom-select').select('Con');
-        cy.get(':nth-child(3) > :nth-child(3) > input').clear('fg');
-        cy.get(':nth-child(3) > :nth-child(3) > input').type('fgfdg');
-        cy.get('.submit').click();
+      })
+    })
+
+describe('input pros and cons', ()=>{
+  it('user can select pro or con',()=>{
+    cy.get(':nth-child(1) > :nth-child(2) > input').click({force: true});
+    cy.get(':nth-child(1) > :nth-child(2) > input').click({force: true});
+    cy.get(':nth-child(2) > :nth-child(2) > input').clear('h');
+    cy.get(':nth-child(2) > :nth-child(2) > input').type('hello');
+    cy.get(':nth-child(2) > :nth-child(2) > .custom-select').select('Pro');
+    cy.get(':nth-child(2) > :nth-child(3) > .custom-select').select('Con');
+    cy.get(':nth-child(2) > :nth-child(3) > input').clear('n');
+    cy.get(':nth-child(2) > :nth-child(3) > input').type('nope');
+    cy.get(':nth-child(3) > :nth-child(2) > .custom-select').select('Pro');
+    cy.get(':nth-child(3) > :nth-child(2) > input').clear('fd');
+    cy.get(':nth-child(3) > :nth-child(2) > input').type('fdgrefdg');
+    cy.get(':nth-child(3) > :nth-child(3) > .custom-select').select('Con');
+    cy.get(':nth-child(3) > :nth-child(3) > input').clear('fg');
+    cy.get(':nth-child(3) > :nth-child(3) > input').type('fgfdg');
+    cy.get('.submit').click();
+  })
+
+})
+   
+    
+    
+
+
+
+
+    describe('temp', () => {
+      it('temp', () => {
+      
+       
+  
         cy.get('.submit').click();
         cy.get(':nth-child(1) > .pro-con-select').select('Mushrooms');
         cy.get(':nth-child(2) > .pro-con-select').select('pepperoni');
