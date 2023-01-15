b7acef5cad169ac793f55612307669ddee43f397
francesco44Hello <francescolongo44@gmail.com>
2023-01-15 15:24:50 +0000

    changed title

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,11 @@
 import Display from '../Display/index';
+import { useEffect } from 'react';
 import './index.css';
 
 function App() {
+   useEffect(() => {
+    document.title = 'Snap-vote';
+  }, []);
   return (
       < >
           <img className='logo' src="https://i.ibb.co/sjwZvkv/SN2.png" alt="img"></img>
